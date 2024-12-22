2a5654c138889bf8a4ec3d5636220bb201eeb7ab
vijayabharathy16 <vijayabharathy753@gmail.com>
2024-12-22 12:49:13 +0000

    first commit

--- shell.sh
+++ shell.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-
 # Variable declaration
 greeting="System Information Script"
 cpu_info=""
@@ -29,3 +28,16 @@ echo "Operating System Version: $os_version"
 # Get Kernel version information
 kernel_version=$(uname -r)
 echo "Kernel Version: $kernel_version"
+
+
+#Output here
+
+
+System Information Script
+CPU Information: Model name:                           AMD EPYC 9354P 32-Core Processor
+Available Disk Space: /dev/sda1        96G  4.2G   92G   5% /
+Available Memory: Mem:           7.8Gi       479Mi       6.8Gi        15Mi       754Mi       7.3Gi
+Operating System Version: PRETTY_NAME="Ubuntu 24.04.1 LTS"
+Kernel Version: 6.8.0-51-generic
+
+
