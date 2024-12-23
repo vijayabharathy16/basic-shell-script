f18c93292b53574c37a6bff2e2f773e9da901902
vijayabharathy16 <vijayabharathy753@gmail.com>
2024-12-23 09:05:41 +0000

    first commit

--- unit-test.sh
+++ unit-test.sh
@@ -30,14 +30,3 @@ kernel_version=$(uname -r)
 echo "Kernel Version: $kernel_version"
 
 
-#Output here
-
-
-System Information Script
-CPU Information: Model name:                           AMD EPYC 9354P 32-Core Processor
-Available Disk Space: /dev/sda1        96G  4.2G   92G   5% /
-Available Memory: Mem:           7.8Gi       479Mi       6.8Gi        15Mi       754Mi       7.3Gi
-Operating System Version: PRETTY_NAME="Ubuntu 24.04.1 LTS"
-Kernel Version: 6.8.0-51-generic
-
-
